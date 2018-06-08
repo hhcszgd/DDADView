d1f540ed04962f99828a305a0710e25ef4be8bf5
wangyuanfei <979433528@qq.com>
2018-06-08 19:05:46 +0800

    first push

---
+++ DDADView/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DDADView
+//
+//  Created by WY on 2018/6/8.
+//  Copyright © 2018年 HHCSZGD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "homePage"
+        self.view.backgroundColor = UIColor.orange
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
